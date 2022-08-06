93ed0cbf370141d29498c2600389748b48603eeb
Resort-Last <kadara666@gmail.com>
2022-08-06 15:44:20 +0200

    Update Notification.py

--- Notification.py
+++ Notification.py
@@ -5,4 +5,3 @@ def send_discord_message(message):
     webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1005086331640234025/KLb-FAYbF3_Wcuwz9HC1PX2RIVPAubQ1Q-dJNmgp8dsqWUYTTBMGaBsa0qBIBGFSjOCv", content=message)
     webhook.execute()
 
-# as
