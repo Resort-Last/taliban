23cc9795e8b63450f2fc9e322f6d0b9850e5163a
Resort-Last <kadara666@gmail.com>
2022-08-09 16:08:48 +0200

    Update backtester.py

--- Backtester/backtester.py
+++ Backtester/backtester.py
@@ -17,53 +17,66 @@ StrategyOne = ta.Strategy(
 class BackTester:
     """TODO:Calculate profits"""
 
-    def __init__(self, strategy, db, interval, start_date, end_date):
+    def __init__(self, strategy, db, interval, start_date, end_date, signals):
         self.strategy = strategy
         self.start_date = start_date
         self.end_date = end_date
         self.dataframe = transform_database(db, interval)
+        self.dataframe.ta.strategy(self.strategy)
+        self.df = self.dataframe.dropna(0)
+        self.signals = signals
+        self.signal_list = []
+        for signal in self.signals:
+            self.signal_list.append(self.ta_lib_calculations(signal))
         self.processed_df = self.apply_strategy()
+
         print(self.processed_df)
 
     def apply_strategy(self):
-        self.dataframe.ta.strategy(self.strategy)
-        df = self.dataframe.dropna(0)
-
-        # ta. number 1
-        df['calc_bool'] = (df['ITS_9'] - df['IKS_26']) >= 0
-        df['ichi_signal'] = df['calc_bool'].shift(1) != df['calc_bool']  # TRUE where there is a cross
-        df.loc[(df['calc_bool'] == True) & (df['ITS_9'] > df[['ISB_26', 'ISA_9']].max(axis=1)) & (
-                    df['ichi_signal'] == True), f'{self.strategy.ta[0]["kind"]}_entry'] = 'BUY'
-        df.loc[(df['calc_bool'] == False) & (df['ITS_9'] < df[['ISB_26', 'ISA_9']].min(axis=1)) & (
-                    df['ichi_signal'] == True), f'{self.strategy.ta[0]["kind"]}_entry'] = 'SELL'
-
-        signal_one = df.dropna(subset=[f'{self.strategy.ta[0]["kind"]}_entry'])
-
-        # ta. number 2
-        df['RSI_calc'] = 0
-        df.loc[(df['RSI_14'] < 20), 'RSI_calc'] = -1
-        df.loc[(df['RSI_14'] > 80), 'RSI_calc'] = 1
-        df.loc[(df['RSI_calc'] == 0) & (df['RSI_calc'].shift(1) == -1), f'{self.strategy.ta[1]["kind"]}_exit'] = 'BUY'
-        df.loc[(df['RSI_calc'] == 0) & (df['RSI_calc'].shift(1) == 1), f'{self.strategy.ta[1]["kind"]}_exit'] = 'SELL'
-        signal_two = df.dropna(subset=[f'{self.strategy.ta[1]["kind"]}_exit'])
-
-        # combined list without the first signal
-
-        signal = signal_one.append(signal_two)
+        signal = pd.DataFrame().append(self.signal_list)
         signal = signal.sort_values(by='Time')
-        signal = signal[['Time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Symbol', f'{self.strategy.ta[0]["kind"]}_entry', f'{self.strategy.ta[1]["kind"]}_exit']]
-
-        # apply time constraints
         if self.start_date:
             signal = signal[(signal['Time'] >= self.start_date)]
         if self.end_date:
             signal = signal[(signal['Time'] <= self.end_date)]
-
         return signal
 
     def calculate_profit(self):
         pass
 
+    # Ta.lib goes here
+    def ta_lib_calculations(self, strat):
+        if strat == 'ichimoku':
+            self.df['calc_bool'] = (self.df['ITS_9'] - self.df['IKS_26']) >= 0
+            self.df['ichi_signal'] = self.df['calc_bool'].shift(1) != self.df['calc_bool']  # TRUE where there is a cross
+            self.df.loc[(self.df['calc_bool'] == True) & (self.df['ITS_9'] > self.df[['ISB_26', 'ISA_9']].max(axis=1)) & (
+                        self.df['ichi_signal'] == True), f'{strat}_entry'] = 'BUY'
+            self.df.loc[(self.df['calc_bool'] == False) & (self.df['ITS_9'] < self.df[['ISB_26', 'ISA_9']].min(axis=1)) & (
+                        self.df['ichi_signal'] == True), f'{strat}_entry'] = 'SELL'
+            signal = self.df.dropna(subset=[f'{strat}_entry'])
+            signal = signal[['Time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Symbol', f'{strat}_entry']]
+            return signal
+
+        elif strat == 'rsi':
+            self.df['RSI_calc'] = 0
+            self.df.loc[(self.df['RSI_14'] < 20), 'RSI_calc'] = -1
+            self.df.loc[(self.df['RSI_14'] > 80), 'RSI_calc'] = 1
+            self.df.loc[(self.df['RSI_calc'] == 0) & (self.df['RSI_calc'].shift(1) == -1), f'{strat}_exit'] = 'BUY'
+            self.df.loc[(self.df['RSI_calc'] == 0) & (self.df['RSI_calc'].shift(1) == 1), f'{strat}_exit'] = 'SELL'
+            signal = self.df.dropna(subset=[f'{strat}_exit'])
+            signal = signal[['Time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Symbol', f'{strat}_exit']]
+            return signal
+
+        elif strat == 'whatever':
+            # do stuff
+            # return signal
+            pass
+
 
 if __name__ == '__main__':
-    dingdong = BackTester(strategy=StrategyOne, db=db_obj, interval=15, start_date='2022-01-01', end_date=None)
+    dingdong = BackTester(strategy=StrategyOne,
+                          db=db_obj,
+                          interval=15,
+                          start_date='2022-01-01',
+                          end_date=None,
+                          signals=['ichimoku', 'rsi'])
