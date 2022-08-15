d6c47ec0d2c90d72036e5f99bbdaa23c0afe18e1
Resort-Last <kadara666@gmail.com>
2022-08-15 08:46:58 +0200

    Update apply_strat.py

--- apply_strat.py
+++ apply_strat.py
@@ -1,12 +1,13 @@
 import pandas_ta as ta
 
 
-StrategyOne = ta.Strategy(
+StrategyReal = ta.Strategy(
     name="Momo and Volatility",
     description="Ichimoku, RSI, MACD",
     ta=[
         {"kind": "ichimoku", "include_chikou": False},
-        {"kind": "bop"}]
+        {"kind": "bop"},
+        {"kind": "macd"}],
 )
 
 
@@ -14,15 +15,23 @@ def strategy(df):
     """call from main, expects a return for entry and/or exit.
     this can either be BUY or SELL or NaN.
     """
-    df.ta.strategy(StrategyOne)
+    df.ta.strategy(StrategyReal)
     # ENTRY
     df['BOP_calc'] = 0
     df['BOP_14'] = 0
     df['BOP_14'] = df['BOP'].rolling(14).mean()
     df.loc[(df['BOP_14'] < 0), 'BOP_calc'] = -1
     df.loc[(df['BOP_14'] > 0), 'BOP_calc'] = 1
-    df.loc[(df['BOP_calc'] == 1) & (df['BOP_calc'].shift(1) == -1), '_entry'] = 'BUY'
-    df.loc[(df['BOP_calc'] == -1) & (df['BOP_calc'].shift(1) == 1), '_entry'] = 'SELL'
+    df.loc[(df['BOP_calc'] == 1) & (df['BOP_calc'].shift(1) == -1), 'bop_entry'] = 'BUY'
+    df.loc[(df['BOP_calc'] == -1) & (df['BOP_calc'].shift(1) == 1), 'bop_entry'] = 'SELL'
+
+    df['macdh_calc'] = (df['MACDh_12_26_9']) >= 0
+    df['macd_signal'] = df['macdh_calc'].shift(1) != df['macdh_calc']
+    df.loc[(df['macdh_calc'] == False) & (df['macd_signal'] == True), f'macd_entry'] = 'SELL'
+    df.loc[(df['macdh_calc'] == True) & (df['macd_signal'] == True), f'macd_entry'] = 'BUY'
+
+    df.loc[(df['bop_entry'] == 'SELL') & (df['macd_entry'] == 'SELL'), '_entry'] = 'SELL'
+    df.loc[(df['bop_entry'] == 'BUY') & (df['macd_entry'] == 'BUY'), '_entry'] = 'BUY'
 
     # EXIT
     df['calc_bool'] = (df['ITS_9'] - df['IKS_26']) >= 0
